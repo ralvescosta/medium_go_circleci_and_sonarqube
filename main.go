9cf22846a754323582ccbab7245703f517c7c4e8
ralvescosta <rafael.rac.mg@gmail.com>
2022-03-20 07:28:21 -0300

    feat: simple project

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"ci/pkg"
+)
+
+func main() {
+	log.Printf("1 + 1 = %v\n", pkg.Sum(1, 1))
+	log.Printf("ABS(-2) = %v\n", pkg.Abs(-2))
 }
---
+++ pkg/math.go
@@ -0,0 +1,13 @@
+package pkg
+
+func Sum(a, b int) int {
+	return a + b
+}
+
+func Abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+
+	return a
+}
